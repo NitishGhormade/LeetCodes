3eaef720890444428fd1112ac5187beaa1470d1f
Nitish Ghormade <dadughormade@gmail.com>
2024-09-17 16:50:49 +0530

    Q78 SubSets Part 2

--- Recursion/78_Subsets.java
+++ Recursion/78_Subsets.java
@@ -1,4 +1,28 @@
+class Solution {
+    static void genSubs(List<List<Integer>> ls, ArrayList<Integer> current, int[] nums, int idx){
+        // base case
+        if(ls.contains(current) == false){
+            ls.add(new ArrayList(current));
+        }
+        if(idx == nums.length){
+            return;
+        }
 
+        current.add(nums[idx]);
+        genSubs(ls, current, nums, idx + 1);
+
+        current.remove(current.size() - 1);
+        genSubs(ls, current, nums, idx + 1);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ls = new ArrayList<>();
+
+        genSubs(ls, new ArrayList<Integer>(), nums, 0);
+
+        return ls;
+    }
+}
 
 ////////////////////////////////////////////////////// Second Solution /////////////////////////////////////////////
 
