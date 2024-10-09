d797023dee07abd34c5a481a9a20198801430c51
Nitish Ghormade <dadughormade@gmail.com>
2024-10-09 22:51:24 +0530

    Q169 Added

--- Arrays/169_Majority_Element.java
+++ Arrays/169_Majority_Element.java
@@ -1,5 +1,19 @@
-package Arrays;
-
-public class 169_Majority_Element {
-    
+class Solution {
+    public int majorityElement(int[] nums) {
+        int num = nums[0];
+        int cnt = 1;
+        for(int i = 1;i < nums.length;i++){
+            if(num == nums[i]){
+                cnt += 1;
+            }
+            else{
+                cnt -= 1;
+            }
+            if(cnt == 0){
+                num = nums[i];
+                cnt = 1;
+            }
+        }
+        return num;
+    }
 }
