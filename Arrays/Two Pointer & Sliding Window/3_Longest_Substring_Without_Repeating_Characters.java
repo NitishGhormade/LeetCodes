224f53fccb2d371d58efc83908521f43007f50d6
Nitish Ghormade <dadughormade@gmail.com>
2024-10-30 23:29:53 +0530

    Q3 Added

--- Arrays/Two Pointer & Sliding Window/3_Longest_Substring_Without_Repeating_Characters.java
+++ Arrays/Two Pointer & Sliding Window/3_Longest_Substring_Without_Repeating_Characters.java
@@ -1,5 +1,30 @@
-package Two Pointer & Sliding Window;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxlen = 0;
 
-public class 3_Longest_Substring_Without_Repeating_Characters {
-    
+        int l = 0;
+        int r = 0;
+        Map<Character, Integer> mp = new HashMap<>();
+        while(r < s.length()){
+            if(mp.get(s.charAt(r)) == null){
+                mp.put(s.charAt(r), r);
+            }
+            else{
+                if(mp.get(s.charAt(r)) < l){
+                    mp.put(s.charAt(r), r);
+                }
+                else{
+                    l = mp.get(s.charAt(r)) + 1;
+                    mp.put(s.charAt(r), r);
+                }
+            }
+
+            if(r - l + 1 > maxlen){
+                maxlen = r - l + 1;
+            }
+
+            r += 1;
+        }
+        return maxlen;
+    }
 }
