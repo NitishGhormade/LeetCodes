fe5a3a08444e3ff3cf549e985e0dcd1648b41cae
Nitish Ghormade <dadughormade@gmail.com>
2024-11-04 18:08:27 +0530

    Q796 Added

---
+++ Strings/796_Rotate_String.java
@@ -0,0 +1,20 @@
+// O( n^2 ) time complexity
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        for(int i = 0;i < s.length();i++){
+            int j;
+            for(j = 0;j < goal.length();j++){
+                if(s.charAt((i + j) % s.length()) != goal.charAt(j)){
+                    break;
+                }
+            }
+
+            if(j == goal.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
