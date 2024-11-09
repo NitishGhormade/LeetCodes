cbe9a8172ad3211c87a92f9c9a5d57579eab7646
Nitish Ghormade <dadughormade@gmail.com>
2024-11-09 13:35:43 +0530

    Q1829 Best Solution Added

--- Arrays/Bit Manipulation/1829_Maximum_XOR_for_Each_Query.java
+++ Arrays/Bit Manipulation/1829_Maximum_XOR_for_Each_Query.java
@@ -1,3 +1,29 @@
+// Optimal Solution, Time Complexity: O(n), Space Complexity: O(n)
+// justLessThanMaxNum has all 1's in binary representation so :- justLessThanMaxNum - xor = A Number with 1's in binary where the XOR Number doesn't have 1's.
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int[] ans = new int[nums.length];
+
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int justLessThanMaxNum = (int)Math.pow(2, maximumBit) - 1;
+        int idx = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int k = justLessThanMaxNum - xor;
+
+            ans[idx++] = k;
+            xor ^= nums[i];
+        }
+
+        return ans;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Developing the Value of k Step by Step from the XOR. Better Solution. Time Complexity: O( n * (totalBits - 1) )
 class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
