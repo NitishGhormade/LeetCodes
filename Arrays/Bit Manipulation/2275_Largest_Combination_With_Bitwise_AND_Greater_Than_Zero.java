cf4bfad6b9b317f08a7a52458adda5c8e1545e48
Nitish Ghormade <dadughormade@gmail.com>
2024-11-09 11:28:57 +0530

    Q2275 Optimized Solution Added

--- Arrays/Bit Manipulation/2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.java
+++ Arrays/Bit Manipulation/2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.java
@@ -1,3 +1,29 @@
+// Time Complexity: O( n * 24 ) 
+// 10^7 needs 24 Bits to represent
+class Solution {
+    public int largestCombination(int[] candidates) {
+        int maxlen = 0;
+
+        int totalBits = (int)Math.floor(Math.log(1e7) / Math.log(2)) + 1; // 24
+
+        for (int i = 0; i < totalBits; i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < candidates.length; j++) {
+                if ((candidates[j] & (1 << i)) > 0) {
+                    cnt++;
+                }
+            }
+
+            maxlen = Math.max(maxlen, cnt);
+        }
+
+        return maxlen;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // O( 2^n ) time complexity, O( n ) space complexity, (Time Limit Exceeded)
 class Solution {
     static int maxlen;
